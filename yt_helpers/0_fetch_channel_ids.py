3e011f53d14438e7c18f7f3e307e3c8bddcb3401
Y3l4n <123765099+Y3l4n@users.noreply.github.com>
2024-12-11 14:38:19 +0700

    Update helpers

--- yt_helpers/0_fetch_channel_ids.py
+++ yt_helpers/0_fetch_channel_ids.py
@@ -2,19 +2,14 @@ import json
 import os
 
 def extract_channel_ids_from_multiple_jsons(json_folder, output_file):
-    """
-    Extracts all channel IDs from multiple JSON files in a directory and saves them into a single text file.
-    (After fetching the trending videos, this will collect all channel ids into channel_ids.txt)
-    (so that you could run the YTstats for each channel in the trending list)
+    '''
+    Fetch all channel ids in the trending videos dataset, then write them to channel_ids.txt
+    This can be used in YTStats to fetch the video info of all channel presented in the trending videos dataset
+    '''
 
-    Parameters:
-    json_folder (str): Path to the folder containing JSON files.
-    output_file (str): Path to the text file where the channel IDs will be saved.
-    """
     all_channel_ids = set()  # Use a set to avoid duplicate channel IDs
 
     try:
-        # Iterate through all files in the folder
         for file_name in os.listdir(json_folder):
             # Process only `.json` files
             if file_name.endswith('.json'):
@@ -25,11 +20,9 @@ def extract_channel_ids_from_multiple_jsons(json_folder, output_file):
                 with open(file_path, 'r', encoding='utf-8') as file:
                     data = json.load(file)
                 
-                # Extract channel IDs and add them to the set
                 channel_ids = [video_data["channelId"] for video_id, video_data in data.items()]
                 all_channel_ids.update(channel_ids)
         
-        # Save all unique channel IDs to the output file
         with open(output_file, 'w', encoding='utf-8') as outfile:
             outfile.write("\n".join(all_channel_ids))
         
@@ -38,6 +31,4 @@ def extract_channel_ids_from_multiple_jsons(json_folder, output_file):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Example usage:
-# Provide the folder containing your JSON files and the output text file
 extract_channel_ids_from_multiple_jsons("newest_trending_videos", "channel_ids.txt")
